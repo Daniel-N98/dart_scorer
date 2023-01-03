0158049041907470a38cc97b057a19935ffcd019
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2023-01-03 18:58:45 +0000

    online-game

--- src/views/Games.jsx
+++ src/views/Games.jsx
@@ -1,7 +1,12 @@
+import CreateOnlineMatch from "../components/CreateOnlineMatch";
+import JoinOnlineMatch from "../components/JoinOnlineMatch";
+
 export default function Games() {
   return (
     <div>
       <h1>Games page</h1>
+      <CreateOnlineMatch />
+      <JoinOnlineMatch />
     </div>
   );
 }
