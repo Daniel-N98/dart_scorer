332a7abd5bd47874c5577f13cddbce98d76e67f4
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2023-01-16 21:44:53 +0000

    Refactored ScoreInput to be reusable

--- src/components/X01Game/ScoreInput.jsx
+++ src/components/X01Game/ScoreInput.jsx
@@ -1,54 +1,33 @@
-import { getAuth } from "firebase/auth";
 import { useState } from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { useParams } from "react-router-dom";
-import {
-  addMatchToCompletedGames,
-  deleteGameDocument,
-  updatePlayerDocument,
-  updatePlayerScore,
-} from "../../firebase";
+import "./styles/scoreInput.css";
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, "Clear", 0, "Submit"];
 
-export default function ScoreInput({ gameRef }) {
-  const [user] = useAuthState(getAuth());
-  const [typedScore, setTypedScore] = useState("");
-  const { gameID } = useParams();
-  const { turn } = gameRef;
+export default function ScoringInput({ setTypedScore }) {
+  const [tempTypedScore, setTempTypedScore] = useState("");
 
-  const updateScore = async (input) => {
-    // Initial score validation
-    if (
-      !validateScore(gameRef, user, input, typedScore, setTypedScore) ||
-      input === "Clear"
-    ) {
-      setTypedScore("");
+  const updateScore = (input) => {
+    if (input === "Clear") {
+      setTempTypedScore("");
       return;
     }
-
-    // Update typedScore state with digit input
-    if (typedScore.length < 3) {
-      setTypedScore((typedScore) => typedScore + input);
-    }
-
-    // Submit score to document
     if (input === "Submit") {
-      const remainingScore = gameRef[turn].score - typedScore;
-      const tempTypedScore = typedScore;
-      setTypedScore("");
-      await handleUpdateScore({
-        tempTypedScore,
-        gameRef,
-        remainingScore,
-        gameID,
-      });
+      setTypedScore(tempTypedScore);
+      setTempTypedScore("");
+      return;
     }
+    if (tempTypedScore.length >= 3) {
+      return;
+    }
+    if (Number(tempTypedScore + input) > 180) {
+      return;
+    }
+    setTempTypedScore((tempTypedScore) => tempTypedScore + input);
   };
 
   return (
     <section id="scorer-input-section" style={{ marginTop: "5px" }}>
-      <h3 id="typed-score">{typedScore}</h3>
+      <h3 id="typed-score">{tempTypedScore}</h3>
       <div id="scorer-input">
         {numbers.map((number) => (
           <button
@@ -63,83 +42,3 @@ export default function ScoreInput({ gameRef }) {
     </section>
   );
 }
-
-function validateScore(gameRef, user, typedScore) {
-  const { turn } = gameRef;
-
-  // Not the users turn, false
-  if (gameRef[turn].uid !== user.uid) {
-    return false;
-  }
-  // Too many points, false
-  if (Number(typedScore) > 180) {
-    return false;
-  }
-
-  // Score validated, true
-  return true;
-}
-
-async function handleUpdateScore({
-  tempTypedScore: score,
-  gameRef,
-  remainingScore,
-  gameID,
-}) {
-  const { turn, sets, legs, start_score } = gameRef;
-  const { sets: pSets, legs: pLegs, name } = gameRef[turn];
-
-  // Remaining score is invalid
-  if (remainingScore < 0 || remainingScore === 1 || remainingScore === "") {
-    return;
-  }
-  // Adds darts score to dart_scores
-  await updatePlayerScore(gameID, score);
-  const updateObject = {};
-  let setWon = false;
-  // Remaining score is valid, leg, set or match has been won.
-  if (remainingScore === 0) {
-    updateObject.p1 = { score: start_score };
-    updateObject.p2 = { score: start_score };
-    // Player has reached the total number of legs in the set
-    if (pLegs + 1 === legs) {
-      // Reset both users legs to 0
-      updateObject.p1.legs = 0;
-      updateObject.p2.legs = 0;
-      setWon = true;
-      updateObject[turn].sets = pSets + 1; // Increase set winners sets by 1
-    } else {
-      updateObject[turn].legs = pLegs + 1; // Increase leg winners legs by 1
-    }
-
-    // Player has reached the total number of sets required to win the match
-    if (setWon && pSets + 1 === sets) {
-      // Add properties required for the end of the match
-      updateObject.winner = name;
-      updateObject.status = "finished";
-      updateObject.gameID = gameID;
-    }
-  } else {
-    // Update players score to the remaining score.
-    updateObject[turn] = { score: remainingScore };
-  }
-  updateObject.turn = turn === "p1" ? "p2" : "p1";
-  await sendUpdate(updateObject, gameID); // Update the players score within the match document
-}
-
-const sendUpdate = async (updateObject, gameID) => {
-  await updatePlayerDocument(gameID, updateObject);
-  // Match has been won
-  if (updateObject.status === "finished") {
-    // Move document to games_finished collection, and delete from games collection
-    try {
-      await addMatchToCompletedGames(gameID);
-      await deleteGameDocument(gameID);
-    } catch (error) {
-      console.error("Unable to complete request", error);
-      return;
-    }
-    // Alter the URL to load the end game screen
-    document.location.href = `/games/online/${gameID}/finished`;
-  }
-};
--- src/components/online/OnlineX01Game.jsx
+++ src/components/online/OnlineX01Game.jsx
@@ -1,15 +1,26 @@
 import { doc, onSnapshot } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { db } from "../../firebase";
-import ScoreInput from "../X01Game/ScoreInput";
+import {
+  addMatchToCompletedGames,
+  db,
+  deleteGameDocument,
+  updatePlayerDocument,
+  updatePlayerScore,
+} from "../../firebase";
+// import ScoreInput from "../X01Game/ScoreInput";
 import ScoreScreen from "../X01Game/ScoreScreen";
 import "../styles/x01Game.css";
+import ScoreInput from "../X01Game/ScoreInput";
+import { getAuth } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 export default function OnlineX01Game() {
+  const [user] = useAuthState(getAuth());
   const { gameID } = useParams();
   const [gameRef, setGameRef] = useState();
   const [loading, setLoading] = useState(false);
+  const [typedScore, setTypedScore] = useState("");
 
   useEffect(() => {
     setLoading(true);
@@ -24,6 +35,22 @@ export default function OnlineX01Game() {
     });
   }, [gameID]);
 
+  useEffect(() => {
+    if (!gameRef) return;
+    const { turn } = gameRef;
+    if (gameRef[turn].uid !== user.uid) return;
+
+    async function handleScoreUpdate() {
+      await updateScore({
+        tempTypedScore: typedScore,
+        gameRef,
+        remainingScore: gameRef[turn].score - typedScore,
+        gameID,
+      });
+    }
+    handleScoreUpdate();
+  }, [typedScore]);
+
   if (loading) return <h2>Loading</h2>;
 
   return (
@@ -34,7 +61,8 @@ export default function OnlineX01Game() {
             <ScoreScreen gameRef={gameRef} player="p1" />
             <ScoreScreen gameRef={gameRef} player="p2" />
           </div>
-          <ScoreInput gameRef={gameRef} />
+          {/* <ScoreInput gameRef={gameRef} /> */}
+          <ScoreInput setTypedScore={setTypedScore} />
         </div>
       ) : (
         <h2>Loading</h2>
@@ -42,3 +70,67 @@ export default function OnlineX01Game() {
     </div>
   );
 }
+
+async function updateScore({
+  tempTypedScore: score,
+  gameRef,
+  remainingScore,
+  gameID,
+}) {
+  const { turn, sets, legs, start_score } = gameRef;
+  const { sets: pSets, legs: pLegs, name } = gameRef[turn];
+
+  // Remaining score is invalid
+  if (remainingScore < 0 || remainingScore === 1 || remainingScore === "") {
+    return;
+  }
+  // Adds darts score to dart_scores
+  await updatePlayerScore(gameID, score);
+  const updateObject = {};
+  let setWon = false;
+  // Remaining score is valid, leg, set or match has been won.
+  if (remainingScore === 0) {
+    updateObject.p1 = { score: start_score };
+    updateObject.p2 = { score: start_score };
+    // Player has reached the total number of legs in the set
+    if (pLegs + 1 === legs) {
+      // Reset both users legs to 0
+      updateObject.p1.legs = 0;
+      updateObject.p2.legs = 0;
+      setWon = true;
+      updateObject[turn].sets = pSets + 1; // Increase set winners sets by 1
+    } else {
+      updateObject[turn].legs = pLegs + 1; // Increase leg winners legs by 1
+    }
+
+    // Player has reached the total number of sets required to win the match
+    if (setWon && pSets + 1 === sets) {
+      // Add properties required for the end of the match
+      updateObject.winner = name;
+      updateObject.status = "finished";
+      updateObject.gameID = gameID;
+    }
+  } else {
+    // Update players score to the remaining score.
+    updateObject[turn] = { score: remainingScore };
+  }
+  updateObject.turn = turn === "p1" ? "p2" : "p1";
+  await sendUpdate(updateObject, gameID); // Update the players score within the match document
+}
+
+const sendUpdate = async (updateObject, gameID) => {
+  await updatePlayerDocument(gameID, updateObject);
+  // Match has been won
+  if (updateObject.status === "finished") {
+    // Move document to games_finished collection, and delete from games collection
+    try {
+      await addMatchToCompletedGames(gameID);
+      await deleteGameDocument(gameID);
+    } catch (error) {
+      console.error("Unable to complete request", error);
+      return;
+    }
+    // Alter the URL to load the end game screen
+    document.location.href = `/games/online/${gameID}/finished`;
+  }
+};
